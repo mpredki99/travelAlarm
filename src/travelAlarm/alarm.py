ec9a666f33d98b5dfa22e3e043d65c81bd053d04
mpredki99 <mpredki99@gmail.com>
2024-11-23 15:49:15 +0000

    v0.11.11

--- src/travelAlarm/alarm.py
+++ src/travelAlarm/alarm.py
@@ -4,26 +4,27 @@ from kivymd.uix.dialog import MDDialog
 
 
 class Alarm:
-    def __init__(self, address, buffer_size, buffer_unit):
-        self.app = MDApp().get_running_app()
-
-        self.address = address
-        self.buffer_size =  buffer_size
-        self.buffer_unit = buffer_unit
-
-        # Build button to close dialog window
-        self.alarm_button = MDFlatButton(
-            text='OK',
-            theme_text_color='Custom',
-            text_color=self.app.theme_cls.primary_color,
-        )
-        # Build dialog window
-        self.alarm_dialog = MDDialog(
-            title='Wake up!',
-            text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
-            buttons=[self.alarm_button]
-        )
-        # Bind button event to close dialog window
-        self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
-
-        self.alarm_dialog.open()
+    pass
+    # def __init__(self, address, buffer_size, buffer_unit):
+    #     self.app = MDApp().get_running_app()
+    #
+    #     self.address = address
+    #     self.buffer_size =  buffer_size
+    #     self.buffer_unit = buffer_unit
+    #
+    #     # Build button to close dialog window
+    #     self.alarm_button = MDFlatButton(
+    #         text='OK',
+    #         theme_text_color='Custom',
+    #         text_color=self.app.theme_cls.primary_color,
+    #     )
+    #     # Build dialog window
+    #     self.alarm_dialog = MDDialog(
+    #         title='Wake up!',
+    #         text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
+    #         buttons=[self.alarm_button]
+    #     )
+    #     # Bind button event to close dialog window
+    #     self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
+    #
+    #     self.alarm_dialog.open()
