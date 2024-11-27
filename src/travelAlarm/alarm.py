fc4730e60718d8010790531a3217fa9e6fafb7eb
mpredki99 <mpredki99@gmail.com>
2024-11-27 20:20:01 +0000

    v0.11.45: Close marker popup on main thread

--- src/travelAlarm/alarm.py
+++ src/travelAlarm/alarm.py
@@ -47,53 +47,9 @@ class Alarm:
         self.vibrate()
 
         # Close map marker popup
-        pin_marker.close_marker_popup()
+        Clock.schedule_once(lambda dt: pin_marker.close_marker_popup(), 0)
 
     def vibrate(self):
         if vibrator.exists():
             for i in range(3):
                 Clock.schedule_once(lambda dt: vibrator.vibrate(1), 1 + i * 2)
-
-
-# class Alarm:
-#     def __init__(self, pin_id, address, buffer_size, buffer_unit):
-#         from kivymd.toast import toast
-#         # Get app instance
-#         self.app = MDApp.get_running_app()
-#
-#         # Deactivate buffer
-#         self.app.pins_db.update_is_active(pin_id, False)
-#
-#         # Close marker popup
-#         self.app.pins_db.pins[pin_id].get('marker').close_marker_popup()
-#
-#         # Set attributes
-#         self.address = address
-#         self.buffer_size =  buffer_size
-#         self.buffer_unit = buffer_unit
-#
-#         # Build button to close dialog window
-#         self.alarm_button = MDFlatButton(
-#             text='OK',
-#             theme_text_color='Custom',
-#             text_color=self.app.theme_cls.primary_color,
-#         )
-#         # Build dialog window
-#         self.alarm_dialog = MDDialog(
-#             title='Wake up!',
-#             text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
-#             buttons=[self.alarm_button]
-#         )
-#         # Bind button event to close dialog window
-#         self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
-#
-#         # Open dialog window
-#         self.alarm_dialog.open()
-#
-#         # Trigger vibrations
-#         self.vibrate()
-#
-#     def vibrate(self):
-#         if vibrator.exists():
-#             for i in range(3):
-#                 Clock.schedule_once(lambda dt: vibrator.vibrate(1), 1 + i * 2)
