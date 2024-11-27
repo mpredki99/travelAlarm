65395ee1cb3e7753409a79c0dcc13c254ae5d6ea
mpredki99 <mpredki99@gmail.com>
2024-11-27 19:38:16 +0000

    v0.11.40: Alarm object get pin item

--- src/travelAlarm/alarm.py
+++ src/travelAlarm/alarm.py
@@ -4,85 +4,89 @@ from kivymd.uix.dialog import MDDialog
 from kivy.clock import Clock
 from plyer import vibrator
 
-
-# class Alarm:
-#     def __init__(self, pin):
-#         from kivymd.toast import toast
-#         # Get app instance
-#         self.app = MDApp.get_running_app()
-#
-#         # Initialize pin object in alarm
-#         self.pin = pin
-#
-#         try:
-#             self.pin.on
-#
-#         # # Build button to close dialog window
-#         # self.alarm_button = MDFlatButton(
-#         #     text='OK',
-#         #     theme_text_color='Custom',
-#         #     text_color=self.app.theme_cls.primary_color,
-#         # )
-#         # # Build dialog window
-#         # self.alarm_dialog = MDDialog(
-#         #     title='Wake up!',
-#         #     text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
-#         #     buttons=[self.alarm_button]
-#         # )
-#         # # Bind button event to close dialog window
-#         # self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
-#         #
-#         # # Open dialog window
-#         # self.alarm_dialog.open()
-#
-#         # Trigger vibrations
-#         # self.vibrate()
-#
-#     # def vibrate(self):
-#     #     if vibrator.exists():
-#     #         for i in range(3):
-#     #             Clock.schedule_once(lambda dt: vibrator.vibrate(1), 1 + i * 2)
+from kivymd.toast import toast
 
 
 class Alarm:
-    def __init__(self, pin_id, address, buffer_size, buffer_unit):
+    def __init__(self, pin):
         from kivymd.toast import toast
         # Get app instance
         self.app = MDApp.get_running_app()
 
-        # Deactivate buffer
-        self.app.pins_db.update_is_active(pin_id, False)
+        # Initialize pin object in alarm
+        self.pin = pin
 
-        # Close marker popup
-        self.app.pins_db.pins[pin_id].get('marker').close_marker_popup()
+        try:
+            self.pin.on_checkbox_click(False)
+        except Exception as e:
+            toast(text=str(e))
 
-        # Set attributes
-        self.address = address
-        self.buffer_size =  buffer_size
-        self.buffer_unit = buffer_unit
-
-        # Build button to close dialog window
-        self.alarm_button = MDFlatButton(
-            text='OK',
-            theme_text_color='Custom',
-            text_color=self.app.theme_cls.primary_color,
-        )
-        # Build dialog window
-        self.alarm_dialog = MDDialog(
-            title='Wake up!',
-            text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
-            buttons=[self.alarm_button]
-        )
-        # Bind button event to close dialog window
-        self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
-
-        # Open dialog window
-        self.alarm_dialog.open()
+        # # Build button to close dialog window
+        # self.alarm_button = MDFlatButton(
+        #     text='OK',
+        #     theme_text_color='Custom',
+        #     text_color=self.app.theme_cls.primary_color,
+        # )
+        # # Build dialog window
+        # self.alarm_dialog = MDDialog(
+        #     title='Wake up!',
+        #     text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
+        #     buttons=[self.alarm_button]
+        # )
+        # # Bind button event to close dialog window
+        # self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
+        #
+        # # Open dialog window
+        # self.alarm_dialog.open()
 
         # Trigger vibrations
-        self.vibrate()
+        # self.vibrate()
 
-    def vibrate(self):
-        if vibrator.exists():
-            for i in range(3):
-                Clock.schedule_once(lambda dt: vibrator.vibrate(1), 1 + i * 2)
+    # def vibrate(self):
+    #     if vibrator.exists():
+    #         for i in range(3):
+    #             Clock.schedule_once(lambda dt: vibrator.vibrate(1), 1 + i * 2)
+
+
+# class Alarm:
+#     def __init__(self, pin_id, address, buffer_size, buffer_unit):
+#         from kivymd.toast import toast
+#         # Get app instance
+#         self.app = MDApp.get_running_app()
+#
+#         # Deactivate buffer
+#         self.app.pins_db.update_is_active(pin_id, False)
+#
+#         # Close marker popup
+#         self.app.pins_db.pins[pin_id].get('marker').close_marker_popup()
+#
+#         # Set attributes
+#         self.address = address
+#         self.buffer_size =  buffer_size
+#         self.buffer_unit = buffer_unit
+#
+#         # Build button to close dialog window
+#         self.alarm_button = MDFlatButton(
+#             text='OK',
+#             theme_text_color='Custom',
+#             text_color=self.app.theme_cls.primary_color,
+#         )
+#         # Build dialog window
+#         self.alarm_dialog = MDDialog(
+#             title='Wake up!',
+#             text=f'You are within {self.buffer_size} {self.buffer_unit} from {self.address}',
+#             buttons=[self.alarm_button]
+#         )
+#         # Bind button event to close dialog window
+#         self.alarm_button.bind(on_press=self.alarm_dialog.dismiss)
+#
+#         # Open dialog window
+#         self.alarm_dialog.open()
+#
+#         # Trigger vibrations
+#         self.vibrate()
+#
+#     def vibrate(self):
+#         if vibrator.exists():
+#             for i in range(3):
+#                 Clock.schedule_once(lambda dt: vibrator.vibrate(1), 1 + i * 2)
--- src/travelAlarm/gpsmarker.py
+++ src/travelAlarm/gpsmarker.py
@@ -230,7 +230,6 @@ class GpsMarker(MapLayer):
 
     def is_within_buffer(self, *args):
         """Check if user is within active buffer and trigger alarm if so."""
-        from kivymd.toast import toast
         # Get pins from database
         pins = self.app.pins_db.pins
 
@@ -240,57 +239,28 @@ class GpsMarker(MapLayer):
         # Create user position tuple
         user_pos = (self.latitude, self.longitude)
 
-        # for pin_id in pins:
-        #     # Skip buffer if pin is not active
-        #     if not pins[pin_id].get('is_active'):
-        #         continue
-        #
-        #     # Create pin position tuple
-        #     pin_pos = (pins[pin_id].get('latitude', 0), pins[pin_id].get('longitude', 0))
-        #
-        #     # Get pin address, buffer size and buffer unit
-        #     address = pins[pin_id].get('address')
-        #     buffer_size = pins[pin_id].get('buffer_size', 0)
-        #     buffer_unit = pins[pin_id].get('buffer_unit')
-        #
-        #     # Convert buffer size to meters
-        #     buffer_meters = buffer_size * unit_mult.get(buffer_unit, 0)
-        #
-        #     # Calculate distance from user to pin
-        #     buffer_distance = geodesic(user_pos, pin_pos).meters
-        #
-        #     # Check if user is within buffer size
-        #     if buffer_distance <= buffer_meters:
-        #         # Create alarm object and trigger alarm
-        #         Clock.schedule_once(lambda dt: Alarm(pin_id, address, buffer_size, buffer_unit), 0)
+        for pin_id in pins:
+            # Skip buffer if pin is not active
+            if not pins[pin_id].get('is_active'):
+                continue
 
-        for attributes in pins.values():
-            pin_marker = attributes.get('marker')
-            try:
-                pin = pin_marker.pin
-                # Skip buffer if pin is not active
-                if not pin.is_active:
-                    continue
+            # Create pin position tuple
+            pin_pos = (pins[pin_id].get('latitude', 0), pins[pin_id].get('longitude', 0))
 
-                # Create pin position tuple
-                pin_pos = (pin.latitude, pin.longitude)
+            # Get pin address, buffer size and buffer unit
+            address = pins[pin_id].get('address')
+            buffer_size = pins[pin_id].get('buffer_size', 0)
+            buffer_unit = pins[pin_id].get('buffer_unit')
 
-                # Get pin address, buffer size and buffer unit
-                address = pin.address
-                buffer_size = pin.buffer_size
-                buffer_unit = pin.buffer_unit
+            # Convert buffer size to meters
+            buffer_meters = buffer_size * unit_mult.get(buffer_unit, 0)
 
-                # Convert buffer size to meters
-                buffer_meters = buffer_size * unit_mult.get(buffer_unit, 0)
+            # Calculate distance from user to pin
+            buffer_distance = geodesic(user_pos, pin_pos).meters
 
-                # Calculate distance from user to pin
-                buffer_distance = geodesic(user_pos, pin_pos).meters
-
-                # Check if user is within buffer size
-                if buffer_distance <= buffer_meters:
-                    pin.on_checkbox_click(False)
-                    # Create alarm object and trigger alarm
-                    # Clock.schedule_once(lambda dt: Alarm(pin), 0)
-
-            except Exception as e:
-                toast(text=e)
+            # Check if user is within buffer size
+            if buffer_distance <= buffer_meters:
+                # Create alarm object and trigger alarm
+                pin_marker = pins[pin_id].get('marker')
+                # Clock.schedule_once(lambda dt: Alarm(pin_id, address, buffer_size, buffer_unit), 0)
+                Clock.schedule_once(lambda dt: Alarm(pin_marker.pin), 0)
