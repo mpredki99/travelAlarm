01527de7b7660b91158996d4d8c85e1a6886c985
mpredki99 <mpredki99@gmail.com>
2024-12-04 19:52:16 +0000

    v1.0.0 First stable version

--- src/travelAlarm/gpsmarker.py
+++ src/travelAlarm/gpsmarker.py
@@ -1,3 +1,9 @@
+# Coding: UTF-8
+
+# Copyright (C) 2024 Michał Prędki
+# Licensed under the GNU General Public License v3.0.
+# Full text of the license can be found in the LICENSE and COPYING files in the repository.
+
 from kivymd.app import MDApp
 from kivy import platform
 from kivy_garden.mapview import MapLayer
